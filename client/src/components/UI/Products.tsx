62f3556f7470e0faa9f7a6f913af9e1a5260c58e
imrany <imranmat254@gmail.com>
2023-07-10 22:33:59 +0300

    worked on products

--- client/src/components/UI/Products.tsx
+++ client/src/components/UI/Products.tsx
@@ -1,3 +1,5 @@
+import image1 from "../../assets/images/blackwomen-massage.jpg";
+import image2 from "../../assets/images/whitelady-face-massage.jpg";
 
 const Products=()=>{
     return(
@@ -6,7 +8,47 @@ const Products=()=>{
             <p className="text-4xl text-center mt-6 w-[50vw] font-semibold text-[#333333]">
                 Our best product this week, that can change your life
             </p>
+            <div className="mt-10 justify-center items-center flex max-md:flex-col gap-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-x-5">
+                <div className="flex flex-col w-[30vw] max-md:w-[50vw]">
+                    <img src={image1} alt="product images" className="max-md:h-[20vh]"/>
+                    <p className="mt-10 text-[#333333]">
+                        Natural Oil
+                    </p>
+                    <p className="mt-2 text-[#808080]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, consequatur hic. Perspiciatis id reprehenderit commodi aspernatur reiciendis error necessitatibus ipsam.
+                    </p>
+                </div>
 
+                <div className="flex flex-col w-[30vw] max-md:w-[50vw]">
+                    <img src={image2} alt="product images" className="max-md:h-[20vh]"/>
+                    <p className="mt-10 text-[#333333]">
+                        Natural Oil
+                    </p>
+                    <p className="mt-2 text-[#808080]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, consequatur hic. Perspiciatis id reprehenderit commodi aspernatur reiciendis error necessitatibus ipsam.
+                    </p>
+                </div>
+
+                <div className="flex flex-col w-[30vw] max-md:w-[50vw]">
+                    <img src={image1} alt="product images" className="max-md:h-[20vh]"/>
+                    <p className="mt-10 text-[#333333]">
+                        Natural Oil
+                    </p>
+                    <p className="mt-2 text-[#808080]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, consequatur hic. Perspiciatis id reprehenderit commodi aspernatur reiciendis error necessitatibus ipsam.
+                    </p>
+                </div>
+
+                <div className="flex flex-col w-[30vw] max-md:w-[50vw]">
+                    <img src={image1} alt="product images" className="max-md:h-[20vh]"/>
+                    <p className="mt-10 text-[#333333]">
+                        Natural Oil
+                    </p>
+                    <p className="mt-2 text-[#808080]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, consequatur hic. Perspiciatis id reprehenderit commodi aspernatur reiciendis error necessitatibus ipsam.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
