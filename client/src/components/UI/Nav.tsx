5aae7c2ba153de61a1067267cd1b54e51838f5f9
imrany <imranmat254@gmail.com>
2023-07-11 16:56:42 +0300

    footer responsiveness

--- client/src/components/UI/Nav.tsx
+++ client/src/components/UI/Nav.tsx
@@ -8,23 +8,29 @@ function Nav() {
     const {email}=useContext(Usercontext)
     
     return (
-        <nav className="flex justify-between py-6 border-b-[1px]" style={{borderBottomColor:"#8o8o8o"}}>
-           <Link to="/" className="font-bold text-2xl text-[#333333]" id='logo'>Spa online</Link>
-           <div className="flex text-base" style={{color:"#808080"}}>
-                <Link to="/" className='hover:text-[#333333]'>Home</Link>
-                <a href="#about" className='hover:text-[#333333] px-4'>About</a>
-                <Link to="/products" className='hover:text-[#333333] px-4'>Products</Link>
-                <Link to="/features" className='hover:text-[#333333]'>Features</Link>
-           </div>
-           <div className="flex text-base" style={{color:"#808080"}}>
-                <Link to="/" className='hover:bg-[#333333] hover:text-[#FFFCF8] hover:rounded-[50px] px-2 py-1'>
-                    <i className='ri-search-line'></i>
-                </Link>
-                <Link to="/" className='hover:bg-[#333333] hover:text-[#FFFCF8] hover:rounded-[50px] px-2 py-1'>
-                    <i className='ri-notification-line'></i>
-                </Link>
-           </div>
-        </nav>
+        <>
+            <nav className="flex justify-between py-6 border-b-[1px]" style={{borderBottomColor:"#8o8o8o"}}>
+                <Link to="/" className="font-bold text-2xl text-[#333333]" id='logo'>Spa online</Link>
+                <div className="flex text-base" style={{color:"#808080"}} id='nav-items'>
+                        <Link to="/" className='hover:text-[#333333]'>Home</Link>
+                        <a href="#about" className='hover:text-[#333333] px-4'>About</a>
+                        <Link to="/products" className='hover:text-[#333333] px-4'>Products</Link>
+                        <Link to="/features" className='hover:text-[#333333]'>Features</Link>
+                </div>
+                <div className="flex text-base" style={{color:"#808080"}} id='nav-items'>
+                        <Link to="/" className='hover:bg-[#333333] hover:text-[#FFFCF8] hover:rounded-[50px] px-2 py-1'>
+                            <i className='ri-search-line'></i>
+                        </Link>
+                        <Link to="/" className='hover:bg-[#333333] hover:text-[#FFFCF8] hover:rounded-[50px] px-2 py-1'>
+                            <i className='ri-notification-line'></i>
+                        </Link>
+                </div>
+
+                <div className="text-[#333333]" id='nav-menu'>
+                    <i className='ri-menu-line ri-xl cursor-pointer'></i>
+                </div>
+            </nav>
+        </>
     );
 };
 
