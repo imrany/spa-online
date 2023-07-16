bec1d07f2c27acd37afc8f6753dd58cb2bb7eb45
imrany <imranmat254@gmail.com>
2023-07-16 20:25:51 +0300

    server admin update

--- server/views/public/main.js
+++ server/views/public/main.js
@@ -1,1 +1,18 @@
+const route=window.location.pathname
+const parseRes=JSON.parse(localStorage.getItem('data'))
 
+switch (route) {
+    case "/":
+        if(parseRes.token){
+            window.location.href="/admin"
+        }
+        break;
+    case "/admin":
+        if(!parseRes){
+            window.location.href="/"
+        }
+        break;
+    default:
+        window.location.href="/"
+        break;
+}
