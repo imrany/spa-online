d45e86a43dbef57c910e8d3beeaf2b5c0bc4779a
imrany <imranmat254@gmail.com>
2023-07-16 20:50:33 +0300

    getting all db collection and displaying it on the admin page

--- server/src/routes/views.ts
+++ server/src/routes/views.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { connection } from "mongoose";
 
 const views=express.Router();
 
@@ -6,7 +7,8 @@ views.get("/",(req:any,res:any)=>{
     res.render("login",{title:"Login"})
 })
 
-views.get("/admin",(req:any,res:any)=>{
-    res.render("index",{title:"Admin"})
+views.get("/admin",async(req:any,res:any)=>{
+    const collection=await Object.keys(connection.collections);
+    res.render("index",{title:"Admin",collections:collection})
 })
 export default views
